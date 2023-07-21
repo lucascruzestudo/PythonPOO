76fa9662c4e44a6d3b8650c980cada61e9a15cfa
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-20 21:31:17 -0300

    Protegendo os atributos (ou quase...)

--- conta.py
+++ conta.py
@@ -2,19 +2,19 @@ class Conta:
 
     def __init__(self, numero, titular, saldo, limite=1000.0):
         print(f"Construiu o objeto {self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo de {self.titular}: R$ {self.saldo}")
+        print(f"Saldo de {self.__titular}: R$ {self.__saldo}")
     
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
 
     
     
