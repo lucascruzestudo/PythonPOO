1b1213e4b31d2b3e147f2db4fd2d3a03f75e1ca8
Lucas Gomes Cruz <lucas.cruz@modalgr.com.br>
2023-07-20 22:42:42 -0300

    Criando getters e setters

--- conta.py
+++ conta.py
@@ -20,6 +20,21 @@ class Conta:
         self.sacar(valor)
         destino.depositar(valor)
 
+    def get_saldo(self):
+        return self.__saldo
+    
+    def get_limite(self):
+        return self.__limite
+    
+    def get_titular(self):
+        return self.__titular
+    
+    def get_numero(self):
+        return self.__numero
+    
+    def set_limite(self, limite):
+        self.__limite = limite
+
     
     
 
